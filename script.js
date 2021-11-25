be3ad82cffc4045e208e7c5c279a2ad59cc0ea26
Anna Ostrowska-Kus <anna.ostrowska@wp.pl>
2021-11-25 21:46:28 +0100

    add axios and async function

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ const img =document.querySelector('img')
 
 const URL = 'https://dog.ceo/api/breeds/image/random'
 
-btn.addEventListener('click', () => {
-    fetch(URL).then(res => res.json()).then(data => img.setAttribute('src', data.message)).catch(err => console.error(err))
-})
+// btn.addEventListener('click', () => {
+//     fetch(URL).then(res => res.json()).then(data => img.setAttribute('src', data.message)).catch(err => console.error(err))
+// })
+
+
+
+async function showImage(){
+    const response = await axios.get(URL)
+    img.setAttribute('src', response.data.message)
+}
+
+btn.addEventListener('click', ()=> showImage())
 
 
 
